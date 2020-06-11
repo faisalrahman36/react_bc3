1ea6690cfedc4eb02ffd5fad24debce54bf5493b
faisalrahman36@hotmail.com <faisalrahman36@hotmail.com>
2020-06-11 20:54:14 +0500

    update bc3

--- src/App.js
+++ src/App.js
@@ -6,5 +6,13 @@ function App(props) {
 return <div>Hello from App.js by Syed Faisal ur Rahman {props.name} for {props.course}
 We can pass a number like {props.number}</div>
 }
+/*
+WE CAN ALSO DO THIS by using destructuring 
+function App(name,number,course) {
+return <div>Hello from App.js by Syed Faisal ur Rahman {name} for {course}
+We can pass a number like {number}</div>
+}
 
+
+*/ 
 export default App;
